a9e29c16bb7d15e0959cad26a7fd1bb97ef3e0b2
Wildan Bash <wildandawambash@gmail.com>
2020-09-05 22:00:14 +0700

    add MyViewAndViews

---
+++ MyViewAndViews/app/src/main/java/com/bash/myviewandviews/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bash.myviewandviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Google Pixel"
+    }
+}
